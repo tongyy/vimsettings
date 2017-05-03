5849ff1c33164e12374787535ba2667a1a2f3473
tony <tony@xxx.com>
2017-05-03 10:11:12 +0800

    eslint plugin

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+npm install -g eslint
+npm install -g babel-eslint
+npm install -g eslint-plugin-react
 cp .vimrc ~/.vimrc
